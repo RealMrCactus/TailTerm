fda6d90008229b4d3028af459a69c5d4d33cc7c3
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-12-09 23:22:19 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,64 @@
-use qt_core::{QString, Slot};
-use qt_widgets::{QApplication, QLineEdit, QPushButton, QVBoxLayout, QWidget};
-use std::process::Command;
-use std::os::unix::process::CommandExt;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::process::Command;
+use qmetaobject::*;
+use nix::pty::{openpty, Winsize};
+use nix::unistd::{fork, ForkResult, setsid, dup2, execvp};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let shell = "/bin/bash"; // This should be replaced with code that finds the user's shell
+#[derive(QObject, Default)]
+struct TerminalWindow {
+    base: qt_base_class!(trait QObject),
+}
 
-    let mut child = Command::new(shell)
-        .spawn()
-        .expect("failed to spawn shell");
+impl TerminalWindow {
+    fn show(&self) {
+        let engine = QmlEngine::new();
+        engine.load_data(r#"
+            import QtQuick 2.0
+            import QtQuick.Controls 2.15
 
-    QApplication::init(|_| unsafe {
-        let widget = QWidget::new_0a();
-        let layout = QVBoxLayout::new_1a(&widget);
-        let line_edit = QLineEdit::new();
-        let button = QPushButton::from_q_string(&QString::from_std_str("Send"));
+            ApplicationWindow {
+                visible: true
+                width: 640
+                height: 480
+                title: qsTr("Rust Terminal Emulator")
 
-        layout.add_widget(&line_edit);
-        layout.add_widget(&button);
+                TextArea {
+                    anchors.fill: parent
+                    font.family: "monospace"
+                    // ... additional properties ...
+                }
+            }
+        "#.into());
 
-        let slot = Slot::new(move || {
-            let command = line_edit.text().to_std_string() + "\n";
-            child.stdin.as_mut().unwrap().write_all(command.as_bytes()).await.unwrap();
-        });
+        engine.exec();
+    }
+}
 
-        button.released().connect(&slot);
+fn spawn_shell() {
+    let pty_master = openpty(None, None).expect("Failed to open PTY");
 
-        widget.show();
-        QApplication::exec()
-    })
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { .. }) => {
+            // Parent process: this will interact with the PTY master
+            // TODO: Read from and write to PTY master
+        },
+        Ok(ForkResult::Child) => {
+            setsid().expect("setsid failed");
+            let pty_slave = pty_master.slave.expect("No PTY slave available");
+
+            dup2(pty_slave.as_raw_fd(), 0).unwrap();
+            dup2(pty_slave.as_raw_fd(), 1).unwrap();
+            dup2(pty_slave.as_raw_fd(), 2).unwrap();
+
+            execvp("sh", &["sh"]).expect("Failed to execute shell");
+        },
+        Err(_) => println!("Fork failed"),
+    }
+}
+
+fn main() {
+    let terminal_window = TerminalWindow::default();
+
+    // Note: In a real application, you'll need to find a way to manage 
+    //       both the Qt event loop and the PTY I/O simultaneously.
+    spawn_shell();
+    terminal_window.show();
 }
