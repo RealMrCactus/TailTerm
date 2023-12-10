a6e75535cfe930fd14fca3d4be32bdc6b5a10334
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-12-10 12:22:35 -0500

    Quick check to see if i fucked up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, TextView};
-use nix::pty::{forkpty, openpty, ptsname, Winsize};
+use nix::pty::{openpty, Winsize};
 use std::os::unix::io::AsRawFd;
+use std::thread;
 
 fn opty() {
     // Attempt to open a new PTY
@@ -13,10 +14,9 @@ fn opty() {
             println!("Master FD: {}", pty.master.as_raw_fd());
             println!("Slave FD: {}", pty.slave.as_raw_fd());
 
-            // You can now read from and write to these file descriptors.
-            // Typically, you might fork the process here, with the child
-            // process becoming a session leader and attaching the slave
-            // end of the PTY to its standard streams.
+            // The PTY handling would go here.
+            // You might read from the master FD and update the TextView in your GTK app,
+            // or take input from the TextView and write it to the master FD.
         }
         Err(e) => {
             eprintln!("Failed to open PTY: {}", e);
@@ -24,30 +24,35 @@ fn opty() {
     }
 }
 
-
 fn main() {
     // Initialize GTK application
-    let application = Application::new(Some("com.realmrcactus.tailterm"), Default::default());
+    if let Ok(application) = Application::new(Some("com.realmrcactus.tailterm"), Default::default()) {
+        application.connect_activate(|app| {
+            // Create a new window
+            let window = ApplicationWindow::new(app);
+            window.set_title("TailTerm");
+            window.set_default_size(850, 450);
 
-    application.connect_activate(|app| {
-        // Create a new window
-        let window = ApplicationWindow::new(app);
-        window.set_title("TailTerm");
-        window.set_default_size(850, 450);
+            // Create a text view (a multi-line text box)
+            let text_view = TextView::new();
+            text_view.set_editable(true); // Make the text view editable
+            text_view.set_wrap_mode(gtk::WrapMode::Word); // Set wrap mode
 
-        // Create a text view (a multi-line text box)
-        let text_view = TextView::new();
-        text_view.set_editable(true); // Make the text view editable
-        text_view.set_wrap_mode(gtk::WrapMode::Word); // Set wrap mode
+            // Add the text view to the window
+            window.add(&text_view);
 
-        // Add the text view to the window
-        window.add(&text_view);
+            // Show all window components
+            window.show_all();
+        });
 
-        // Show all window components
-        window.show_all();
-    });
+        // Start the PTY handling in a separate thread
+        thread::spawn(move || {
+            opty();
+        });
 
-    // Run the application
-    opty();
-    application.run();
+        // Run the application
+        application.run();
+    } else {
+        eprintln!("Failed to initialize GTK application");
+    }
 }
