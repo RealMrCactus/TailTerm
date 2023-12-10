c3a4e34b4b3eb51875facb93ace81b45aaa0e88a
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-12-10 14:58:16 -0500

    Whopps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, TextView, TextBuffer, glib};
+use glib::source;
+use nix::pty::openpty;
+use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+use std::{io::Read, thread, sync::mpsc};
+use std::sync::{Arc, Mutex};
+
+fn setup_pty_output_to_textview(master_fd: RawFd, text_view: TextView, tx: mpsc::Sender<String>) {
+    thread::spawn(move || {
+        // SAFETY: We're assuming here that we're the only ones who have access to this FD.
+        let mut master_file = unsafe { std::fs::File::from_raw_fd(master_fd) };
+        let mut buffer = [0; 1024];
+
+        loop {
+            match master_file.read(&mut buffer) {
+                Ok(size) => {
+                    if size > 0 {
+                        let output = String::from_utf8_lossy(&buffer[..size]).to_string();
+                        tx.send(output).expect("Failed to send output to main thread");
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error reading from PTY: {}", e);
+                    break;
+                }
+            }
+        }
+    });
+}
+
 fn main() {
     // Initialize GTK application
     let application = Application::new(Some("com.example.tailterm"), Default::default());
